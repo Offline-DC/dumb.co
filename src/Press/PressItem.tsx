bd2cbee89346f27a3d42894ed79a34052e87ff38
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2026-01-08 15:03:57 -0500

    Working Press page

--- src/Press/PressItem.tsx
+++ src/Press/PressItem.tsx
@@ -16,7 +16,7 @@ type Props = {
 };
 
 export default function PressItem({ item, isHighlighted }: Props) {
-  const { title, source, image, href } = item;
+  const { title, source, image } = item;
 
   const rowClassName = [
     styles.itemRow,
@@ -26,7 +26,7 @@ export default function PressItem({ item, isHighlighted }: Props) {
     .join(" ");
 
   return (
-    <a className={rowClassName} href={href} target="_blank" rel="noreferrer">
+    <div className={rowClassName}>
       <img className={styles.thumb} src={image} alt={title} />
 
       <div className={styles.itemContent}>
@@ -35,6 +35,6 @@ export default function PressItem({ item, isHighlighted }: Props) {
           {source ? <span className={styles.source}> — {source}</span> : null}
         </div>
       </div>
-    </a>
+    </div>
   );
 }
--- src/Press/press_data.md
+++ src/Press/press_data.md
@@ -1,9 +1,14 @@
-id: wapo-mo
+id: nbc-news
 image_name: article_one.webp
-title: this is your title
-href: url.com
+title: is it cringe to be online? yes. go dumb • nbc news
+href: https://www.nbcnews.com/pop-culture/pop-culture-news/going-analog-2025-trends-phone-free-parties-junk-journals-rcna248499
 
-id: vox-example
+id: wapo
 image_name: imrs.webp
-title: this is your other title
-href: url2.com
+title: can u be dumb 4 1 month? • wash post
+href: https://www.washingtonpost.com/podcasts/post-reports/could-you-give-up-your-smartphone-for-a-month/
+
+id: hushharbor-1
+image_name: wiz-primary.svg
+title: hush harbor black owned no phones • nba
+href: https://www.nba.com/wizards/videos/black-owned-dmv-hush-harbor
