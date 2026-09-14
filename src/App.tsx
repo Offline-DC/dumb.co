c77ad25ea2cd4d00625772f27ff3a053e3b28e39
eaintm <EAINTPHOOMAY@GMAIL.COM>
2026-09-14 10:53:27 -0400

    Revert "Merge pull request #6 from Offline-DC/dumbdown-redirect"
    This reverts commit b20f8ec09e34e0d6e934dd61ee9c2de21205c03b, reversing
    changes made to bde1e95f6a000606782041c03c7aba05cdc839f4.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 
 import Phone from "./Phone/Phone";
 import OfflineMode from "./OfflineMode";
+import DumbDumb from "./DumbDumb.tsx";
 import Support from "./Support";
 import Internship from "./Internship/Internship.tsx";
 import NotFound from "./NotFound";
@@ -60,7 +61,7 @@ function App() {
         <Routes>
           <Route element={<CenteredShell />}>
             <Route path="/" element={<Phone />} />
-            <Route path="/dumbdown" element={<AppRedirect />} />
+            <Route path="/dumbdown" element={<DumbDumb />} />
             <Route path="/setup" element={<OfflineMode />} />
             <Route path="/support" element={<Support />} />
             <Route path="/internship" element={<Internship />} />
